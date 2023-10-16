f3245f49c7a3cb2ba00c2fd9d7b1b188b24aa32e
ZUXRIDDIN <zuxriddinzarifov25@gmail.com>
2023-10-16 18:28:49 +0500

    migrations

---
+++ PictureSharing/Migrations/20231016131601_Initial.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace PictureSharing.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.EnsureSchema(
+                name: "PictureSharing");
+
+            migrationBuilder.CreateTable(
+                name: "friends",
+                schema: "PictureSharing",
+                columns: table => new
+                {
+                    id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    from_id = table.Column<long>(type: "bigint", nullable: false),
+                    to_id = table.Column<long>(type: "bigint", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_friends", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "users",
+                schema: "PictureSharing",
+                columns: table => new
+                {
+                    id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    password = table.Column<string>(type: "text", nullable: false),
+                    email = table.Column<string>(type: "text", nullable: false),
+                    created_at = table.Column<DateTime>(type: "timestamp without time zone", nullable: false),
+                    updated_at = table.Column<DateTime>(type: "timestamp without time zone", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_users", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "clients",
+                schema: "PictureSharing",
+                columns: table => new
+                {
+                    id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    name = table.Column<string>(type: "text", nullable: false),
+                    surname = table.Column<string>(type: "text", nullable: false),
+                    user_id = table.Column<long>(type: "bigint", nullable: false),
+                    created_at = table.Column<DateTime>(type: "timestamp without time zone", nullable: false),
+                    updated_at = table.Column<DateTime>(type: "timestamp without time zone", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_clients", x => x.id);
+                    table.ForeignKey(
+                        name: "FK_clients_users_user_id",
+                        column: x => x.user_id,
+                        principalSchema: "PictureSharing",
+                        principalTable: "users",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "photos",
+                schema: "PictureSharing",
+                columns: table => new
+                {
+                    id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    path = table.Column<string>(type: "text", nullable: false),
+                    root_photo = table.Column<string>(type: "text", nullable: false),
+                    client_id = table.Column<long>(type: "bigint", nullable: false),
+                    created_at = table.Column<DateTime>(type: "timestamp without time zone", nullable: false),
+                    updated_at = table.Column<DateTime>(type: "timestamp without time zone", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_photos", x => x.id);
+                    table.ForeignKey(
+                        name: "FK_photos_clients_client_id",
+                        column: x => x.client_id,
+                        principalSchema: "PictureSharing",
+                        principalTable: "clients",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_clients_user_id",
+                schema: "PictureSharing",
+                table: "clients",
+                column: "user_id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_photos_client_id",
+                schema: "PictureSharing",
+                table: "photos",
+                column: "client_id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "friends",
+                schema: "PictureSharing");
+
+            migrationBuilder.DropTable(
+                name: "photos",
+                schema: "PictureSharing");
+
+            migrationBuilder.DropTable(
+                name: "clients",
+                schema: "PictureSharing");
+
+            migrationBuilder.DropTable(
+                name: "users",
+                schema: "PictureSharing");
+        }
+    }
+}
